5180e6602aadea9cafb0c8fe3ad4dd1b0026ba92
jawwad9 <jawwadmuhammad514@gmail.com>
2024-09-29 23:50:54 +0500

    Commit

--- src/config/firebase/firebaseconfig.js
+++ src/config/firebase/firebaseconfig.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage,  } from "firebase/storage";
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyA6k8IEesl5N3NhRfveoclg8N7Q1pQ8blc",
@@ -15,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, query, where } from "firebase/firestore"; 
+import { db } from '../config/firebase/firebaseconfig';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../config/firebase/firebaseconfig';
 
 const Profile = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const checkUser = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("Authenticated User UID:", uid);
+        await UserData(uid);
+      } else {
+        console.log("User not signed in");
+      }
+    });
+    return () => checkUser();
+  }, []);
+
+  const UserData = async (uid) => {
+    const q = query(collection(db, "User"), where("UserId", "==", uid)); 
+    const querySnapshot = await getDocs(q);
+    
+    console.log("Query Snapshot:", querySnapshot);
+
+    if (!querySnapshot.empty) {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`); 
+        setData(doc.data());
+      });
+    } else {
+      console.log("No user data found for this UID.");
+    }
+  };
+
   return (
-    <>
-        <div>Profile</div>
+    <div className="bg-white min-h-screen flex items-center justify-center">
+      {data ? (
+        <div className='flex flex-col items-center gap-2'>
+          <h1 className='text-2xl'>First Name: {data.FirstName}</h1>
+          <h1 className='text-2xl'>Last Name: {data.LastName}</h1>
+          <h1 className='text-2xl'>Email: {data.Email}</h1>
+          <img style={{width: "14rem"}} className='rounded-lg' src={data.ImageUrl} alt="Profile" />
+        </div>
+      ) : (
+        <h1 className='text-center'>Loading...</h1>
+      )}
+    </div>
+  );
+};
 
-
-    </>
-  )
-}
-
-export default Profile
+export default Profile;
